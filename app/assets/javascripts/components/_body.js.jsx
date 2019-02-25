a4484f6984e973a3933e79a3224363fa6ba822f2
Stella Pickard <stella@pickard.co.za>
2019-02-25 12:06:31 +0200

    CRUD actions working

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -8,6 +8,28 @@ class Body extends React.Component {
       this.addNewFruit = this.addNewFruit.bind(this)  
       this.handleDelete = this.handleDelete.bind(this)
       this.deleteFruit = this.deleteFruit.bind(this)
+      this.handleUpdate = this.handleUpdate.bind(this)
+      this.updateFruit = this.updateFruit.bind(this)
+  }
+
+  handleUpdate(fruit){
+    fetch('http://localhost:3000/api/v1/fruits/${fruit.id}', 
+    {
+      method: 'PUT',
+      body: JSON.stringify({fruit: fruit}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => { 
+        this.updateFruit(fruit)
+      })
+  }
+  updateFruit(fruit){
+    let newFruits = this.state.fruits.filter((f) => f.id !== fruit.id)
+    newFruits.push(fruit)
+    this.setState({
+      fruits: newFruits
+    })
   }
 
   handleDelete(id){
@@ -59,7 +81,7 @@ class Body extends React.Component {
       return(
         <div>
           <NewFruit handleFormSubmit={this.handleFormSubmit}/>
-          <AllFruits fruits={this.state.fruits} handleDelete={this.handleDelete}/>
+          <AllFruits fruits={this.state.fruits} handleDelete = {this.handleDelete} handleUpdate = {this.handleUpdate }/>
         </div>
       )
     }
